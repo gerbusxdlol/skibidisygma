17092f7b57c91e901b5d0d225f7fe991a1d342cf
gerbusxdlol <gerbusxdlol@onet.pl>
2025-05-02 18:31:59 +0200

    Add files via upload

---
+++ elkopelko123.js
@@ -0,0 +1,7 @@
+<script>
+  for (let i = 2; i < 100; i++) {
+    const snowflake = document.createElement('div');
+    snowflake.classList.add('snowflake');
+    document.querySelector('.snow').appendChild(snowflake);
+  }
+</script>
