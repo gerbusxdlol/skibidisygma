93cddd8b9503278b24540486f97a8e0efce0809d
gerbusxdlol <gerbusxdlol@onet.pl>
2025-05-03 22:03:46 +0200

    Delete public directory

--- public/elkopelko123.js
+++
@@ -1,7 +0,0 @@
-<script>
-  for (let i = 2; i < 100; i++) {
-    const snowflake = document.createElement('div');
-    snowflake.classList.add('snowflake');
-    document.querySelector('.snow').appendChild(snowflake);
-  }
-</script>
