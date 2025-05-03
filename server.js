1507956ee0de33d059cc117d879836a52bf7a3d9
gerbusxdlol <gerbusxdlol@onet.pl>
2025-05-03 21:33:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcryptjs");
+app.use(express.static('public'));
 
 const app = express();
 app.get('/', (req, res) => {
